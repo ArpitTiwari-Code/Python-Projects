54fcf6a5a2f24489dc08265d9145796209d9fd94
Arpit Tiwari <arpittiwari4559@gmail.com>
2024-05-25 12:11:01 +0530

    Add files via upload

---
+++ Fabbacci.py
@@ -0,0 +1,12 @@
+#Fabbancci series
+c=int(input("enter the how many time you want the fabbacci seq"))
+a=0
+b=1
+d=0
+print(a)
+for i in range (0,c):
+
+    d=a+b
+    b=a
+    a=d
+    print(d,end=" "  )
